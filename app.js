2232f47177bacd54d9a5ed36d3ed0d6c847a48f3
selbstdenker <jeangoetten@hotmail.com>
2019-06-07 12:51:52 -0300

    wip

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+
+app.get('/', (req, res)=>{
+    res.send('Olá, mundo!')
+})
+
+const PORT = 8088
+app.listen(PORT, ()=>{
+    console.log("Olá. O Servidor está funcionando na porta " + PORT)
+})
